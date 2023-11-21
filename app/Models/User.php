c03616ae971a71c82444da94716711b71fcaff32
triemaryanto <triemaryanto93@gmail.com>
2023-11-21 12:00:04 +0700

    Model & Database

--- app/Models/User.php
+++ app/Models/User.php
@@ -51,4 +51,9 @@ class User extends Authenticatable implements Auditable
       protected $appends = [
         'profile_photo_url',
     ];
+
+     public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
 }
