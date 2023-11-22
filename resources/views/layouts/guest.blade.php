b224c197816a0aea98cac8af5bf80c5fc8514fdb
triemaryanto <triemaryanto93@gmail.com>
2023-11-22 14:57:54 +0700

    up

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,27 +1,64 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link rel="shortcut icon" type="image/x-icon" href="images/favicon.png" />
 
-        <!-- Styles -->
-        @livewireStyles
-    </head>
-    <body>
-        <div class="font-sans text-gray-900 antialiased">
-            {{ $slot }}
-        </div>
+    <!-- Themefisher Icon font -->
+    <link rel="stylesheet" href="{{ asset('landing-page') }}/plugins/themefisher-font/style.css">
+    <!-- bootstrap.min css -->
+    <link rel="stylesheet" href="{{ asset('landing-page') }}/plugins/bootstrap/css/bootstrap.min.css">
 
-        @livewireScripts
-    </body>
+    <!-- Animate css -->
+    <link rel="stylesheet" href="{{ asset('landing-page') }}/plugins/animate/animate.css">
+    <!-- Slick Carousel -->
+    <link rel="stylesheet" href="{{ asset('landing-page') }}/plugins/slick/slick.css">
+    <link rel="stylesheet" href="{{ asset('landing-page') }}/plugins/slick/slick-theme.css">
+    <link rel="stylesheet" href="{{ asset('landing-page') }}/css/style.css">
+    <!-- Main Stylesheet -->
+
+    <!-- Scripts -->
+    @vite(['resources/js/app.js'])
+</head>
+
+<body id="body">
+    <section class="signin-page account">
+
+        {{ $slot }}
+
+    </section>
+
+    <!-- Main jQuery -->
+    <script src="{{ asset('landing-page') }}/plugins/jquery/dist/jquery.min.js"></script>
+    <!-- Bootstrap 3.1 -->
+    <script src="{{ asset('landing-page') }}/plugins/bootstrap/js/bootstrap.min.js"></script>
+    <!-- Bootstrap Touchpin -->
+    <script src="{{ asset('landing-page') }}/plugins/bootstrap-touchspin/dist/jquery.bootstrap-touchspin.min.js"></script>
+    <!-- Instagram Feed Js -->
+    <script src="{{ asset('landing-page') }}/plugins/instafeed/instafeed.min.js"></script>
+    <!-- Video Lightbox Plugin -->
+    <script src="{{ asset('landing-page') }}/plugins/ekko-lightbox/dist/ekko-lightbox.min.js"></script>
+    <!-- Count Down Js -->
+    <script src="{{ asset('landing-page') }}/plugins/syo-timer/build/jquery.syotimer.min.js"></script>
+
+    <!-- slick Carousel -->
+    <script src="{{ asset('landing-page') }}/plugins/slick/slick.min.js"></script>
+    <script src="{{ asset('landing-page') }}/plugins/slick/slick-animation.min.js"></script>
+
+    <!-- Google Mapl -->
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCC72vZw-6tGqFyRhhg5CkF2fqfILn2Tsw"></script>
+    <script type="text/javascript" src="{{ asset('landing-page') }}/plugins/google-map/gmap.js"></script>
+
+    <!-- Main Js File -->
+    <script src="{{ asset('landing-page') }}/js/script.js"></script>
+</body>
+
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Livewire\Admin\Page\Home;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
 
@@ -27,7 +28,5 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+    Route::get('/dashboard', Home::class)->name('dashboard');
 });
