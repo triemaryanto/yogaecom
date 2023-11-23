153c8764d4cb4d1665fa559a50cec4c33ef23f6f
triemaryanto <triemaryanto93@gmail.com>
2023-11-23 09:27:08 +0700

    dashboard

--- resources/views/components/layouts/admin/app.blade.php
+++ resources/views/components/layouts/admin/app.blade.php
@@ -1,134 +1,107 @@
 <!DOCTYPE html>
+<!--
+Author: Keenthemes
+Product Name: Metronic - Bootstrap 5 HTML, VueJS, React, Angular & Laravel Admin Dashboard Theme
+Purchase: https://1.envato.market/EA4JP
+Website: http://www.keenthemes.com
+Contact: support@keenthemes.com
+Follow: www.twitter.com/keenthemes
+Dribbble: www.dribbble.com/keenthemes
+Like: www.facebook.com/keenthemes
+License: For each use you must have a valid license purchased only from above link in order to legally use the theme for your project.
+-->
 <html lang="en">
+<!--begin::Head-->
 
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta property="og:title" content="">
-    <meta property="og:type" content="">
-    <meta property="og:url" content="">
-    <meta property="og:image" content="">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('ecom_frontend/assets/imgs/template/pemda.png') }}" />
-    <link href="{{ asset('ecom_backend/') }}/assets/css/style.css?v=1.0.0" rel="stylesheet">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-tagsinput/0.8.0/bootstrap-tagsinput.css"
-        rel="stylesheet" />
-    <title>AYOPROMO - Dashboard</title>
+    <base href="">
+    <title>Metronic - the world's #1 selling Bootstrap Admin Theme Ecosystem for HTML, Vue, React, Angular &amp; Laravel
+        by Keenthemes</title>
+    <meta name="description"
+        content="The most advanced Bootstrap Admin Theme on Themeforest trusted by 94,000 beginners and professionals. Multi-demo, Dark Mode, RTL support and complete React, Angular, Vue &amp; Laravel versions. Grab your copy now and get life-time updates for free." />
+    <meta name="keywords"
+        content="Metronic, bootstrap, bootstrap 5, Angular, VueJs, React, Laravel, admin themes, web design, figma, web development, free templates, free admin themes, bootstrap theme, bootstrap template, bootstrap dashboard, bootstrap dak mode, bootstrap button, bootstrap datepicker, bootstrap timepicker, fullcalendar, datatables, flaticon" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta charset="utf-8" />
+    <meta property="og:locale" content="en_US" />
+    <meta property="og:type" content="article" />
+    <meta property="og:title"
+        content="Metronic - Bootstrap 5 HTML, VueJS, React, Angular &amp; Laravel Admin Dashboard Theme" />
+    <meta property="og:url" content="https://keenthemes.com/metronic" />
+    <meta property="og:site_name" content="Keenthemes | Metronic" />
+    <link rel="canonical" href="https://preview.keenthemes.com/metronic8" />
+    <link rel="shortcut icon" href="{{ asset('admin-page/') }}/assets/media/logos/favicon.ico" />
+    <!--begin::Fonts-->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
+    <!--end::Fonts-->
+    <!--begin::Page Vendor Stylesheets(used by this page)-->
+    <link href="{{ asset('admin-page/') }}/assets/plugins/custom/fullcalendar/fullcalendar.bundle.css" rel="stylesheet"
+        type="text/css" />
+    <!--end::Page Vendor Stylesheets-->
+    <!--begin::Global Stylesheets Bundle(used by all pages)-->
+    <link href="{{ asset('admin-page/') }}/assets/plugins/global/plugins.bundle.css" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('admin-page/') }}/assets/css/style.bundle.css" rel="stylesheet" type="text/css" />
+    <!--end::Global Stylesheets Bundle-->
     @vite(['resources/js/app.js'])
-    <script src="{{ asset('ecom_backend/assets/ckeditor/ckeditor.js') }}"></script>
     @stack('css')
-    <!-- Styles -->
     @livewireStyles
 </head>
+<!--end::Head-->
+<!--begin::Body-->
 
-<body>
-    <div class="screen-overlay"></div>
-    @include('layouts.backend.sidebar')
-    <main class="main-wrap">
-        @include('layouts.backend.header')
-        {{ $slot ?? '' }}
-        @include('layouts.backend.footer')
-    </main>
-
+<body id="kt_body"
+    class="header-fixed header-tablet-and-mobile-fixed toolbar-enabled toolbar-fixed toolbar-tablet-and-mobile-fixed aside-enabled aside-fixed"
+    style="--kt-toolbar-height:55px;--kt-toolbar-height-tablet-and-mobile:55px">
+    <!--begin::Main-->
+    <!--begin::Root-->
+    <div class="d-flex flex-column flex-root">
+        <!--begin::Page-->
+        <div class="page d-flex flex-row flex-column-fluid">
+            <!--begin::Aside-->
+            @include('components.layouts.admin.aside')
+            <!--begin::Wrapper-->
+            <div class="wrapper d-flex flex-column flex-row-fluid" id="kt_wrapper">
+                <!--begin::Header-->
+                @include('components.layouts.admin.header')
+                <!--end::Header-->
+                <!--begin::Content-->
+                <div class="content d-flex flex-column flex-column-fluid" id="kt_content">
+                    {{ $slot ?? '' }}
+                </div>
+                <!--end::Content-->
+                <!--begin::Footer-->
+                @include('components.layouts.admin.footer')
+                <!--end::Footer-->
+            </div>
+            <!--end::Wrapper-->
+            <!--end::Aside-->
+        </div>
+        <!--end::Page-->
+    </div>
+    <!--end::Root-->
+    <!--end::Scrolltop-->
+    <!--end::Main-->
     @livewireScripts
-
-    <script src="{{ asset('ecom_backend/') }}/assets/js/vendors/jquery-3.6.0.min.js"></script>
-    <script src="{{ asset('ecom_backend/') }}/assets/js/vendors/bootstrap.bundle.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-tagsinput/0.8.0/bootstrap-tagsinput.js"></script>
-    <script src="{{ asset('ecom_backend/') }}/assets/js/vendors/perfect-scrollbar.js"></script>
-    <script src="{{ asset('ecom_backend/') }}/assets/js/vendors/jquery.fullscreen.min.js"></script>
-    <script src="{{ asset('ecom_backend/') }}/assets/js/vendors/chart.js"></script>
-    <script src="{{ asset('ecom_backend/') }}/assets/js/main.js?v=1.0.0"></script>
-    <script src="{{ asset('ecom_backend/') }}/assets/js/custom-chart.js" type="text/javascript"></script>
-    <script src="{{ asset('costum/select2/js/select2.min.js') }}"></script>
-    <script src="{{ asset('costum/select2/js/form-select2.js') }}"></script>
     @stack('js')
-    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script>
-        window.addEventListener('Delete', event => {
-            Swal.fire(
-                'Deleted!', 'Data has been deleted.', 'success'
-            )
-        });
-        window.addEventListener('Success', event => {
-            Swal.fire(
-                'Good job!', 'Data has been added.', 'success'
-            )
-        });
-        window.addEventListener('Update', event => {
-            Swal.fire(
-                'Good job!', 'Data has been updated.', 'success'
-            )
-        });
-        window.addEventListener('Error', event => {
-            Swal.fire(
-                'Warning !', 'Data Not Excecution.', 'error'
-            )
-        });
-
-        window.addEventListener('swal:modal', event => {
-            Swal.fire({
-                icon: event.detail.type, // Jenis alert
-                title: event.detail.title, // Judul pesan
-                text: event.detail.text, // Isi pesan
-                toast: true,
-                position: 'top-right',
-                showConfirmButton: false,
-                timer: 1500,
-                timerProgressBar: true
-            });
-        });
+        var hostUrl = "{{ asset('admin-page/') }}/assets/";
     </script>
-    <script>
-        document.addEventListener('livewire:load', function() {
-            Livewire.on('confirmRestore', (url) => {
-                Swal.fire({
-                    title: 'Restore.',
-                    text: "Apakah akan Restore data tersebut ?",
-                    type: 'warning',
-                    showCancelButton: true,
-                    confirmButtonColor: '#3085d6',
-                    cancelButtonColor: '#d33',
-                    confirmButtonText: 'Restore!'
-                }).then((result) => {
-                    if (result.value) {
-                        Livewire.emit('restoreRecord', url);
-                    }
-                });
-            });
-        });
-        document.addEventListener('livewire:load', function() {
-            Livewire.on('confirmDelete', (url) => {
-                Swal.fire({
-                    title: 'Delete Permanen.',
-                    text: "Apakah akan delete secara permanen? Proses ini tidak bisa di ulangi.",
-                    type: 'warning',
-                    showCancelButton: true,
-                    confirmButtonColor: '#3085d6',
-                    cancelButtonColor: '#d33',
-                    confirmButtonText: 'Delete Permanen!'
-                }).then((result) => {
-                    if (result.value) {
-                        Livewire.emit('deleteRecord', url);
-                    }
-                });
-            });
-        });
-        document.addEventListener('livewire:load', function() {
-            Livewire.on('showNotification', ($message) => {
-                Swal.fire({
-                    title: 'Info Masseh !.',
-                    text: $message,
-                    type: 'warning',
-                    confirmButtonColor: '#3085d6',
-                    confirmButtonText: 'Siap Masseh.'
-                })
-            });
-        });
-    </script>
-
+    <!--begin::Javascript-->
+    <!--begin::Global Javascript Bundle(used by all pages)-->
+    <script src="{{ asset('admin-page/') }}/assets/plugins/global/plugins.bundle.js"></script>
+    <script src="{{ asset('admin-page/') }}/assets/js/scripts.bundle.js"></script>
+    <!--end::Global Javascript Bundle-->
+    <!--begin::Page Vendors Javascript(used by this page)-->
+    <script src="{{ asset('admin-page/') }}/assets/plugins/custom/fullcalendar/fullcalendar.bundle.js"></script>
+    <!--end::Page Vendors Javascript-->
+    <!--begin::Page Custom Javascript(used by this page)-->
+    <script src="{{ asset('admin-page/') }}/assets/js/custom/widgets.js"></script>
+    <script src="{{ asset('admin-page/') }}/assets/js/custom/apps/chat/chat.js"></script>
+    <script src="{{ asset('admin-page/') }}/assets/js/custom/modals/create-app.js"></script>
+    <script src="{{ asset('admin-page/') }}/assets/js/custom/modals/upgrade-plan.js"></script>
+    <!--end::Page Custom Javascript-->
+    <!--end::Javascript-->
 </body>
+<!--end::Body-->
 
 </html>
