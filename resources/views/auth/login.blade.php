1901f21510947d91c1fb08a088916cb581e59b20
mnurcholis <chollez69@gmail.com>
2023-11-25 23:47:45 +0700

    udah ngantuk, wkwk

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,43 +1,86 @@
 <x-guest-layout>
-    <x-authentication-card>
-        <x-slot name="logo">
-            <x-authentication-card-logo />
-        </x-slot>
 
-        <x-validation-errors class="mb-4" />
+    <!-- breadcrumb start -->
+    <div class="breadcrumb-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="page-title">
+                        <h2>customer's login</h2>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <nav aria-label="breadcrumb" class="theme-breadcrumb">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                            <li class="breadcrumb-item active">login</li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- breadcrumb End -->
 
-        @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ session('status') }}
-            </div>
-        @endif
 
-        <h2 class="text-center">Welcome Back</h2>
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
-            <div class="form-group">
-                <input id="email" type="email" class="form-control" placeholder="Email" name="email"
-                    :value="old('email')" required autofocus autocomplete="username">
-            </div>
-            <div class="form-group">
-                <input id="password" type="password" class="form-control" placeholder="Password" name="password"
-                    required autocomplete="current-password">
-            </div>
-            <div class="text-center">
-                <x-button class="ms-4">
-                    {{ __('Login') }}
-                </x-button>
-            </div>
-        </form>
 
-        <p class="mt-20">Tidak punya Akun ?<a href="{{ route('register') }}"> daftar</a></p>
-        <p class="mt-20">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                    href="{{ route('password.request') }}">
-                    {{ __('Lupa Password?') }}
-                </a>
-            @endif
-        </p>
-    </x-authentication-card>
+    <!--section start-->
+    <section class="login-page section-b-space">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6">
+
+                    <h3>Login</h3>
+                    <div class="theme-card">
+
+                        <x-validation-errors class="mb-4" />
+
+                        @if (session('status'))
+                            <div class="mb-4 font-medium text-sm text-green-600">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+                        <form method="POST" action="{{ route('login') }}" class="theme-form">
+                            @csrf
+                            <div class="form-group">
+                                <label for="email">Email</label>
+                                <input id="email" type="email" class="form-control" placeholder="Email"
+                                    name="email" :value="old('email')" required autofocus autocomplete="email">
+                            </div>
+                            <div class="form-group">
+                                <label for="review">Password</label>
+                                <input id="review" type="password" class="form-control" placeholder="Password"
+                                    name="password" required autocomplete="current-password">
+                            </div>
+
+                            <x-button class="btn btn-solid">
+                                {{ __('Login') }}
+                            </x-button>
+                        </form>
+                        <br>
+                        <br>
+                        <p>
+                            @if (Route::has('password.request'))
+                                <a href="{{ route('password.request') }}">
+                                    {{ __('Lupa Password?') }}
+                                </a>
+                            @endif
+                        </p>
+                    </div>
+                </div>
+                <div class="col-lg-6 right-login">
+                    <h3>New Customer</h3>
+                    <div class="theme-card authentication-right">
+                        <h6 class="title-font">Create A Account</h6>
+                        <p>Sign up for a free account at our store. Registration is quick and easy. It allows you to
+                            be
+                            able to order from our shop. To start shopping click register.</p><a
+                            href="{{ route('register') }}"" class="btn btn-solid">Create an Account</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--Section ends-->
+
 </x-guest-layout>
